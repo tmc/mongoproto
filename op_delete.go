c42de7914ad9ca872116bdd7afa487aa5df391bd
Tim Vaillancourt <tim@elementspace.com>
2016-11-15 21:59:26 +0100

    Added FromReader(), String() and OpCode() funcs to op_delete.go and op_update.go

--- op_delete.go
+++ op_delete.go
@@ -1,6 +1,13 @@
 package mongoproto
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"fmt"
+	"io"
+
+	"github.com/mongodb/mongo-tools/common/bsonutil"
+	"github.com/mongodb/mongo-tools/common/json"
+	"gopkg.in/mgo.v2/bson"
+)
 
 const (
 	OpDeleteSingleRemove OpDeleteFlags = 1 << iota
@@ -14,5 +21,51 @@ type OpDelete struct {
 	Header             MsgHeader
 	FullCollectionName string // "dbname.collectionname"
 	Flags              OpDeleteFlags
-	Selector           *bson.D // the query to select the document(s)
+	Selector           []byte // the query to select the document(s)
+}
+
+func (op *OpDelete) String() string {
+	var query interface{}
+	if err := bson.Unmarshal(op.Selector, &query); err != nil {
+		return "(error unmarshalling)"
+	}
+	queryAsJSON, err := bsonutil.ConvertBSONValueToJSON(query)
+	if err != nil {
+		return fmt.Sprintf("ConvertBSONValueToJSON err: %#v - %v", op, err)
+	}
+	asJSON, err := json.Marshal(queryAsJSON)
+	if err != nil {
+		return fmt.Sprintf("json marshal err: %#v - %v", op, err)
+	}
+	return fmt.Sprintf("OpDelete %v %v", op.FullCollectionName, string(asJSON))
+}
+
+func (op *OpDelete) OpCode() OpCode {
+	return OpCodeDelete
+}
+
+func (op *OpDelete) FromReader(r io.Reader) error {
+	var b [4]byte
+	_, err := io.ReadFull(r, b[:])
+	if err != nil {
+		return err
+	}
+	op.Flags = OpDeleteFlags(getInt32(b[:], 0))
+	name, err := readCStringFromReader(r)
+	if err != nil {
+		return err
+	}
+	op.FullCollectionName = string(name)
+	op.Selector, err = ReadDocument(r)
+	if err != nil {
+		return err
+	}
+	if int(op.Header.MessageLength) > len(op.Selector) + len(op.FullCollectionName) + 1 + 8 + MsgHeaderLen {
+		data, err := ReadDocument(r)
+		if err != nil {
+			return err
+		}
+		op.Selector = append(op.Selector, data...)
+	}
+	return nil
 }
--- op_update.go
+++ op_update.go
@@ -1,6 +1,13 @@
 package mongoproto
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/mongodb/mongo-tools/common/bsonutil"
+	"gopkg.in/mgo.v2/bson"
+)
 
 const (
 	OpUpdateUpsert OpUpdateFlags = 1 << iota
@@ -15,6 +22,66 @@ type OpUpdate struct {
 	Header             MsgHeader
 	FullCollectionName string // "dbname.collectionname"
 	Flags              OpUpdateFlags
-	Selector           *bson.D // the query to select the document
-	Update             *bson.D // specification of the update to perform
+	Selector           []byte // the query to select the document
+	Update             []byte // specification of the update to perform
+}
+
+func (op *OpUpdate) OpCode() OpCode {
+	return OpCodeUpdate
+}
+
+func (op *OpUpdate) String() string {
+	var doc interface{}
+	if err := bson.Unmarshal(op.Selector, &doc); err != nil {
+		return "(error unmarshalling selector data)"
+	}
+	selectorJsonDoc, err := bsonutil.ConvertBSONValueToJSON(doc)
+	if err != nil {
+		return fmt.Sprintf("ConvertBSONValueToJSON err: %#v - %v", op, err)
+	}
+	selectorAsJSON, err := json.Marshal(selectorJsonDoc)
+	if err != nil {
+		return fmt.Sprintf("json marshal err: %#v - %v", op, err)
+	}
+	if err := bson.Unmarshal(op.Update, &doc); err != nil {
+		return "(error unmarshalling update data)"
+	}
+	updateJsonDoc, err := bsonutil.ConvertBSONValueToJSON(doc)
+	if err != nil {
+		return fmt.Sprintf("ConvertBSONValueToJSON err: %#v - %v", op, err)
+	}
+	updateAsJSON, err := json.Marshal(updateJsonDoc)
+	if err != nil {
+		return fmt.Sprintf("json marshal err: %#v - %v", op, err)
+	}
+	return fmt.Sprintf("OpUpdate %v %v %v", op.FullCollectionName, string(selectorAsJSON), string(updateAsJSON))
+}
+
+func (op *OpUpdate) FromReader(r io.Reader) error {
+	var b [4]byte
+	_, err := io.ReadFull(r, b[:])
+	if err != nil {
+		return err
+	}
+	op.Flags = OpUpdateFlags(getInt32(b[:], 0))
+	name, err := readCStringFromReader(r)
+	if err != nil {
+		return err
+	}
+	op.FullCollectionName = string(name)
+	op.Selector, err = ReadDocument(r)
+	if err != nil {
+		return err
+	}
+	var selectorExtra []byte
+	selectorExtra, err = ReadDocument(r)
+	if err != nil {
+		return err
+	}
+	op.Selector = append(op.Selector, selectorExtra...)
+	op.Update, err = ReadDocument(r)
+	if err != nil {
+		return err
+	}
+	return nil
 }
